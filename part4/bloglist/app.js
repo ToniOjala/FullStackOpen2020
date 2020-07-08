d55d3b8fe0dd14f44af467fa95d66e2199368cc2
Toni Ojala <toni.ojala05@gmail.com>
2020-07-08 15:55:44 +0300

    Finish 4.2 Blog list, step2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,31 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const { MONGODB_URI } = require('./utils/config')
+
+logger.info('connecting to ', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('Error while connecting to MongoDB: ', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app

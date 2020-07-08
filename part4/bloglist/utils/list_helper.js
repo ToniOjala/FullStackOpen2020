44a8356f7c70fb4dc2455db71f7156960631bfec
Toni Ojala <toni.ojala05@gmail.com>
2020-07-08 21:08:05 +0300

    Finish 4.5: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,8 +1,8 @@
-const dummy = (blogs) => {
+const dummy = blogs => {
   return 1
 }
 
-const totalLikes = (blogs) => {
+const totalLikes = blogs => {
   let sum = 0
 
   blogs.forEach(blog => {
@@ -12,7 +12,22 @@ const totalLikes = (blogs) => {
   return sum
 }
 
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) return {}
+
+  let mostLiked = blogs.reduce((a, b) => {
+    return (a.likes > b.likes) ? a : b
+  })
+
+  delete mostLiked._id
+  delete mostLiked.__v
+  delete mostLiked.url
+
+  return mostLiked
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
 }

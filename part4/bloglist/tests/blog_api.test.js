b211551a206fba8900a65d8106b6d22a9e97c915
Toni Ojala <toni.ojala05@gmail.com>
2020-07-09 15:49:28 +0300

    Finish 4.10: Blog list tests, step3

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -4,41 +4,41 @@ const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
+const initialBlogs = [ 
+  { 
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+  },
+  {
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0, 
+  },
+  {
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+  }
+]
+
 beforeEach(async () => {
   await Blog.deleteMany()
-
-  const initialBlogs = [ 
-    { 
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-    },
-    {
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-    },
-    {
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-    },
-    {
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0, 
-    },
-    {
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-    }
-  ]
 
   const blogObjects = initialBlogs
     .map(blog => new Blog(blog))
@@ -55,7 +55,7 @@ test('blogs are returned as json', async () => {
 
 test('correct amount of blogs are returned', async () => {
   const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(5)
+  expect(response.body).toHaveLength(initialBlogs.length)
 })
 
 test('blogs have property named id', async () => {
@@ -63,6 +63,28 @@ test('blogs have property named id', async () => {
   response.body.forEach(blog => {
     expect(blog.id).toBeDefined()
   })
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: "Some random blog",
+    author: "Some random author",
+    url: "https:/www.randomblogbyrandomauthor.com",
+    likes: 8429
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const blogsAtEnd = response.body
+  expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1)
+  
+  const titles = blogsAtEnd.map(blog => blog.title)
+  expect(titles).toContain(newBlog.title)
 })
 
 afterAll(() => {

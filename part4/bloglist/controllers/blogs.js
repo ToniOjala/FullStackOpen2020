ee1758874b0de5f60a1a283f54c899dea79402b6
Toni Ojala <toni.ojala05@gmail.com>
2020-07-10 22:05:05 +0300

    Finish 4.20: bloglist expansion, step8

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,15 +3,6 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
- const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7) 
-  }
-
-  return null
- }
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { name: 1, username: 1, id: 1 })
   response.json(blogs)
@@ -19,12 +10,11 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  console.log(request.token)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
   if (!body.title && !body.url) return response.status(400).json({ error: 'title or url missing' })
-
-  if (!token || !decodedToken.id) return response.status(401).json({ error: 'token missing or invalid' })
+  if (!decodedToken.id) return response.status(401).json({ error: 'token missing or invalid' })
 
   const user = await User.findById(decodedToken.id)
   const blog = new Blog({

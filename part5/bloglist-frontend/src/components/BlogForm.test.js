668d15581421f5679065a9a51e295e68d38b4406
Toni Ojala <toni.ojala05@gmail.com>
2020-07-15 11:46:21 +0300

    Finish 5.16: Blog list tests, step4

---
+++ part5/bloglist-frontend/src/components/BlogForm.test.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent } from '@testing-library/react'
+import BlogForm from './BlogForm'
+
+describe('<BlogForm />', () => {
+  test('create blog event handler is called with correct data', () => {
+    const createBlog = jest.fn()
+
+    const component = render(
+      <BlogForm createBlog={createBlog} />
+    )
+
+    const title = component.container.querySelector('#title')
+    const author = component.container.querySelector('#author')
+    const url = component.container.querySelector('#url')
+    const form = component.container.querySelector('form')
+
+    fireEvent.change(title, {
+      target: { value: 'Test Title' }
+    })
+
+    fireEvent.change(author, {
+      target: { value: 'Test Author' }
+    })
+
+    fireEvent.change(url, {
+      target: { value: 'www.testurl.com' }
+    })
+
+    fireEvent.submit(form)
+
+    expect(createBlog.mock.calls).toHaveLength(1)
+    expect(createBlog.mock.calls[0][0].title).toBe('Test Title')
+    expect(createBlog.mock.calls[0][0].author).toBe('Test Author')
+    expect(createBlog.mock.calls[0][0].url).toBe('www.testurl.com')
+  })
+})

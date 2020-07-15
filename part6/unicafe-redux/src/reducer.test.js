280203c6eb33dc2048b2208b5db59657ca797e81
Toni Ojala <toni.ojala05@gmail.com>
2020-07-15 21:50:20 +0300

    Finish 6.1: unicafe revisited, step1

--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -10,18 +10,14 @@ describe('unicafe reducer', () => {
 
   test('should return a proper initial state when called with undefined state', () => {
     const state = {}
-    const action = {
-      type: 'DO_NOTHING'
-    }
+    const action = { type: 'DO_NOTHING' }
 
     const newState = counterReducer(undefined, action)
     expect(newState).toEqual(initialState)
   })
 
   test('good is incremented', () => {
-    const action = {
-      type: 'GOOD'
-    }
+    const action = { type: 'GOOD' }
     const state = initialState
 
     deepFreeze(state)
@@ -31,5 +27,44 @@ describe('unicafe reducer', () => {
       ok: 0,
       bad: 0
     })
+  })
+
+  test('OK is incremented', () => {
+    const action = { type: 'OK' }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = { type: 'BAD' }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('zero returns initial state', () => {
+    const action = { type: 'ZERO' }
+    const state = {
+      good: 5,
+      ok: 8,
+      bad: 2
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual(initialState)
   })
 })

d1c55bee8af300d0e71eff2da28e8e39e59000bb
Toni Ojala <toni.ojala05@gmail.com>
2020-07-15 11:30:39 +0300

    Finish 5.14: Blog list tests, step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,23 +1,50 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('initial rendering is correct', () => {
-  const blog = {
-    title: 'test title',
-    author: 'test author',
-    url: 'test url',
-    likes: 5
-  }
+describe('<Blog />', () => {
 
-  const component = render(
-    <Blog blog={blog} />
-  )
+  let blog, component
 
-  const titleAndAuthor = component.getByText(`${blog.title} - by ${blog.author}`)
+  beforeEach(() => {
+    blog = {
+      title: 'test title',
+      author: 'test author',
+      url: 'test url',
+      likes: 5,
+      user: {
+        name: 'Test User',
+        username: 'testuser'
+      }
+    }
 
-  expect(titleAndAuthor).toBeDefined()
-  expect(component.container).not.toHaveTextContent(blog.url)
-  expect(component.container).not.toHaveTextContent('likes')
+    const loggedInUser = {
+      name: 'Test User',
+      username: 'testuser'
+    }
+
+    component = render(
+      <Blog blog={blog} loggedInUser={loggedInUser} />
+    )
+  })
+
+  test('initial rendering is correct', () => {
+    const titleAndAuthor = component.getByText(`${blog.title} - by ${blog.author}`)
+
+    expect(titleAndAuthor).toBeDefined()
+    expect(component.container).not.toHaveTextContent(blog.url)
+    expect(component.container).not.toHaveTextContent('likes')
+  })
+
+  test('url and likes are rendered after contents are toggled', () => {
+    const button = component.getByText('Show')
+    fireEvent.click(button)
+
+    const url = component.getByText(blog.url)
+    const likes = component.getByText(`likes ${blog.likes}`)
+
+    expect(url).toBeDefined()
+    expect(likes).toBeDefined()
+  })
 })

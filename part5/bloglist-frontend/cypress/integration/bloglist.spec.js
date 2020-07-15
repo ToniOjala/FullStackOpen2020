2fa6eaddb25929dc4ee3916d44c6f06a225c2233
Toni Ojala <toni.ojala05@gmail.com>
2020-07-15 14:03:47 +0300

    Finish 5.18: bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/integration/bloglist.spec.js
+++ part5/bloglist-frontend/cypress/integration/bloglist.spec.js
@@ -1,6 +1,11 @@
 describe('BlogList app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users', {
+      name: 'Matti Meikäläinen',
+      username: 'matmei',
+      password: 'sala123'
+    })
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +14,25 @@ describe('BlogList app', function() {
     cy.contains('username')
     cy.contains('password')
     cy.contains('Log In')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('matmei')
+      cy.get('#password').type('sala123')
+      cy.get('#login-button').click()
+
+      cy.contains('Logged in as Matti Meikäläinen')
+    })
+
+    it('fails with incorrect credentials', function() {
+      cy.get('#username').type('matmei')
+      cy.get('#password').type('väärä123')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'Wrong credentials')
+        .and('have.css', 'color', 'rgb(139, 0, 0)')
+    })
   })
 })

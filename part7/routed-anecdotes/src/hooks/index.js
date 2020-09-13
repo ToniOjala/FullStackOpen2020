9dcaed6af300674816e30024f72ba2dcb662c1cc
Toni Ojala <toni.ojala05@gmail.com>
2020-09-13 11:22:53 +0300

    Finish 7.6: anecdotes and hooks, step3

--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -4,13 +4,14 @@ export const useField = (type) => {
   const [value, setValue] = useState('')
 
   const onChange = (event) => setValue(event.target.value)
-
   const reset = () => setValue('')
 
   return {
-    type,
-    value,
-    onChange,
+    field: { 
+      type,
+      value,
+      onChange
+    },
     reset
   }
 }

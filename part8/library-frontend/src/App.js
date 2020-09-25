eb7f0ed12fb2ec8d8a9180611d14ba2ec08ebba1
Toni Ojala <toni.ojala05@gmail.com>
2020-09-25 21:59:05 +0300

    Finish 8.18: Log in

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,19 +1,51 @@
 
 import React, { useState } from 'react'
+import { useApolloClient } from '@apollo/client'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
 
-const App = () => {
-  const [page, setPage] = useState('authors')
+const Nav = ({ loggedIn, setPage, logout }) => {
 
-  return (
-    <div>
+  if (loggedIn) {
+    return (
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={logout}>logout</button>
       </div>
+    )
+  } else {
+    return (
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('login')}>login</button>
+      </div>
+    )
+  }
+}
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  return (
+    <div>
+      <Nav 
+        loggedIn={token !== null}
+        setPage={setPage}
+        logout={logout}
+      />
 
       <Authors
         show={page === 'authors'}
@@ -25,6 +57,11 @@ const App = () => {
 
       <NewBook
         show={page === 'add'}
+      />
+
+      <LoginForm
+        show={page === 'login'}
+        setToken={setToken}
       />
 
     </div>

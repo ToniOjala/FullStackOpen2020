8d2efdde9cc100029db48a843c4a2e7f50520ada
Toni Ojala <toni.ojala05@gmail.com>
2020-09-27 09:11:42 +0300

    Finish 9.5: WebBMI

--- part9/first-steps/bmiCalculator.ts
+++ part9/first-steps/bmiCalculator.ts
@@ -1,4 +1,4 @@
-const calculateBmi = (height:number, weight:number):string => {
+export const calculateBmi = (height:number, weight:number):string => {
   height = height / 100;
   const bmi = weight / (height*height);  
 
@@ -8,25 +8,25 @@ const calculateBmi = (height:number, weight:number):string => {
   else return 'Very high (obese)';
 }
 
-interface BodyMeasures {
-  height:number,
-  weight:number
-}
+// interface BodyMeasures {
+//   height:number,
+//   weight:number
+// }
 
-const parseArguments = (args: Array<string>):BodyMeasures => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
+// const parseArguments = (args: Array<string>):BodyMeasures => {
+//   if (args.length < 4) throw new Error('Not enough arguments');
+//   if (args.length > 4) throw new Error('Too many arguments');
 
-  if ( !isNaN(Number(args[2])) && !isNaN(Number(args[3])) ) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3])
-    }
-  } else {
-    throw new Error('Provided values were not numbers!');
-  }
-}
+//   if ( !isNaN(Number(args[2])) && !isNaN(Number(args[3])) ) {
+//     return {
+//       height: Number(args[2]),
+//       weight: Number(args[3])
+//     }
+//   } else {
+//     throw new Error('Provided values were not numbers!');
+//   }
+// }
 
-const { height, weight } = parseArguments(process.argv);
+// const { height, weight } = parseArguments(process.argv);
 
-console.log(calculateBmi(height, weight));
+// console.log(calculateBmi(height, weight));
--- part9/first-steps/index.ts
+++ part9/first-steps/index.ts
@@ -1,8 +1,29 @@
-import express from 'express'
+import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+
 const app = express();
 
 app.get('/hello', (_request, response) => {
   response.send('Hello Full Stack!');
+})
+
+app.get('/bmi', (request, response) => {
+  const height = Number(request.query.height);
+  const weight = Number(request.query.weight);
+
+  if ( !height || !weight || isNaN(height) || isNaN(weight) ) {
+    response.send({
+      error: 'malformatted parameters'
+    })
+  }
+
+  const bmi = calculateBmi(height, weight);
+
+  response.send({
+    weight,
+    height,
+    bmi
+  })
 })
 
 const PORT = 3003;

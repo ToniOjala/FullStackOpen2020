8eb64cdf05f1ce424015425ddf3a7dd80e6d30d1
Toni Ojala <toni.ojala05@gmail.com>
2020-09-27 12:30:42 +0300

    9.9: Patientor backend, step2

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.get('/ping', (_request, response) => {
+app.get('/api/ping', (_request, response) => {
   console.log('ping pong');
   response.send('pong');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running in port ${PORT}`);

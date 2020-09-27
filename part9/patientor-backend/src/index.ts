949a199fa9a20ca8ca40ced30ad88a8a0e6335d1
Toni Ojala <toni.ojala05@gmail.com>
2020-09-27 13:22:38 +0300

    Finish 9.10: Patientor backend, step3

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosisRouter from './routes/diagnoses';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -8,6 +10,8 @@ app.get('/api/ping', (_request, response) => {
   console.log('ping pong');
   response.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
 
 const PORT = 3001;
 

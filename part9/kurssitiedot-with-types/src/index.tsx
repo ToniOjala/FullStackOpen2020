a8e07ecd4abeb31aaeeefdd1a959552e3ef718dd
Toni Ojala <toni.ojala05@gmail.com>
2020-09-28 20:14:29 +0300

    Finish 9.15

--- part9/kurssitiedot-with-types/src/index.tsx
+++ part9/kurssitiedot-with-types/src/index.tsx
@@ -1,24 +1,95 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header: React.FC<{ courseName: string }> = ({ courseName }) => (
-  <h1>{courseName}</h1>
-) 
-
-interface Course {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
 }
 
-const Content: React.FC<{ courseParts: Array<Course> }> = ({ courseParts }) => (
+interface CoursePartWithDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartOne extends CoursePartWithDescription {
+  name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartWithDescription {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+
+interface CoursePartFour extends CoursePartWithDescription {
+  name: "Fun with Typescript";
+  groupProjectCount: number;
+  exerciseSubmissionLink: string;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree | CoursePartFour;
+
+const Header: React.FC<{ courseName: string }> = ({ courseName }) => (
+  <h1>{courseName}</h1>
+) 
+
+const Part: React.FC<{ coursePart: CoursePart }> = ({ coursePart }) => {
+  const assertNever = (value: never): never => {
+    throw new Error(`Unhandled discriminated union member: ${JSON.stringify(value)}`);
+  };
+
+  switch(coursePart.name) {
+    case "Fundamentals":
+      return (
+        <div>
+          <h4>{coursePart.name}</h4>
+          <div>{coursePart.exerciseCount}</div>
+          <div>{coursePart.description}</div>
+        </div>
+      )
+    case "Using props to pass data":
+      return (
+        <div>
+          <h4>{coursePart.name}</h4>
+          <div>{coursePart.exerciseCount}</div>
+          <div>{coursePart.groupProjectCount}</div>
+        </div>
+      )
+    case "Deeper type usage":
+      return (
+        <div>
+          <h4>{coursePart.name}</h4>
+          <div>{coursePart.description}</div>
+          <div>{coursePart.exerciseSubmissionLink}</div>
+        </div>
+      )
+    case "Fun with Typescript":
+      return (
+        <div>
+          <h4>{coursePart.name}</h4>
+          <div>{coursePart.exerciseCount}</div>
+          <div>{coursePart.description}</div>
+          <div>{coursePart.groupProjectCount}</div>
+          <div>{coursePart.exerciseSubmissionLink}</div>
+        </div>
+      )
+    default:
+      return assertNever(coursePart)
+  }
+}
+
+const Content: React.FC<{ courseParts: Array<CoursePart> }> = ({ courseParts }) => (
   <div>
-    {courseParts.map( (cp: Course) => 
-      <p key={cp.name}>{cp.name} {cp.exerciseCount}</p>
+    {courseParts.map( (cp: CoursePart) => 
+      <Part key={cp.name} coursePart={cp} />
     )}
   </div>
 )
 
-const Total: React.FC<{ courseParts: Array<Course> }> = ({ courseParts }) => (
+const Total: React.FC<{ courseParts: Array<CoursePart> }> = ({ courseParts }) => (
   <p>
     Number of exercises{" "}
     {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
@@ -27,18 +98,29 @@ const Total: React.FC<{ courseParts: Array<Course> }> = ({ courseParts }) => (
 
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
+    },
+    {
+      name: "Fun with Typescript",
+      exerciseCount: 23,
+      description: "So much fun",
+      groupProjectCount: 2,
+      exerciseSubmissionLink: "https://fake-stuff.io"
     }
   ];
 
